800ca716a68bc2274f39bcad5469469b629c8ae2
husse <hussein.g.abouzeid@gmail.c>
2023-01-30 22:14:17 -0500

    Added Delete feature and Database

---
+++ ToDoApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Todo
+# Create your views here.
+def index(request):
+    todo = Todo.objects.all()
+    if request.method == 'POST':
+        new_todo = Todo(title = request.POST['title'])
+        new_todo.save()
+        return redirect('/')
+    return render(request,'index.html',{'todos' : todo})
+
+def delete(request,pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/')
--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ToDoApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ToDoList.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'ToDoList.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ToDoList',
+        'USER':'postgres',
+        'PASSWORD':'Hussein2001',
+        'HOST':'localhost'
     }
 }
 
